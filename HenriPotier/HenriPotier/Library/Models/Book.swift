ba9a40faa66fb303613805cb0c4ecbd9f61f9a79
Aurélien Haie <>
2021-01-17 22:10:11 +0100

    [REFACTOR] Setup a MVC pattern folder rearrangement

---
+++ HenriPotier/HenriPotier/Library/Models/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  HenriPotier
+//
+//  Created by Aurélien Haie on 17/01/2021.
+//
+
+import Foundation
+
+struct Book: Decodable {
+    let isbn: String
+    let title: String
+    let price: Int
+    let cover: String
+    let synopsis: [String]
+}
--- HenriPotier/HenriPotier/Library/Views/BookCell.swift
+++ HenriPotier/HenriPotier/Library/Views/BookCell.swift
@@ -1,94 +1,11 @@
 //
-//  LibraryViewController.swift
+//  BookCell.swift
 //  HenriPotier
 //
 //  Created by Aurélien Haie on 17/01/2021.
 //
 
 import UIKit
-
-struct Book: Decodable {
-    let isbn: String
-    let title: String
-    let price: Int
-    let cover: String
-    let synopsis: [String]
-}
-
-class LibraryViewController: UIViewController {
-
-    // MARK: Properties
-
-    let cellId = "cellId"
-
-    // MARK: View elements
-
-    lazy var collectionView: UICollectionView = {
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
-        cv.backgroundColor = UIColor(named: "backgroundColor")
-        cv.dataSource = self
-        cv.delegate = self
-        cv.showsVerticalScrollIndicator = false
-        cv.register(BookCell.self, forCellWithReuseIdentifier: cellId)
-        return cv
-    }()
-
-    // MARK: Lifecycle
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupViews()
-        fetchBooks()
-    }
-
-    // MARK: Custom funcs
-
-    private func setupViews() {
-        view.addSubview(collectionView)
-        collectionView.anchor(to: view)
-    }
-
-    private func fetchBooks() {
-        Network.fetchBooks { (books) in
-            //
-        } failure: {
-            print("bad")
-        }
-
-    }
-
-}
-
-// MARK: Collection View
-extension LibraryViewController: UICollectionViewDataSource {
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! BookCell
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return .extraLargeSpace
-    }
-
-}
-
-extension LibraryViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width - 2 * .extraLargeSpace, height: 180)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: .extraLargeSpace, left: .extraLargeSpace, bottom: 0, right: .extraLargeSpace)
-    }
-
-}
-
 
 class BookCell: UICollectionViewCell {
 
