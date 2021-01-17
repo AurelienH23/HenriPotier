6e94af9c4e3ef81526f7fecbc21688d5e06db8f4
Aurélien Haie <>
2021-01-17 20:22:31 +0100

    Setup the project

---
+++ HenriPotier/HenriPotier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HenriPotier
+//
+//  Created by Aurélien Haie on 17/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
