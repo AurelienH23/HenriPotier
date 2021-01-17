3cfc32ae704236514a4213dfe2fd3cb285283621
Aurélien Haie <>
2021-01-17 22:33:24 +0100

    [REFACTOR] Add ValueButton

--- HenriPotier/HenriPotier/Library/Views/BookCell.swift
+++ HenriPotier/HenriPotier/Library/Views/BookCell.swift
@@ -121,31 +121,3 @@ class BookCell: UICollectionViewCell {
     }
 
 }
-
-
-class ValueButton: UIButton {
-
-    // MARK: Lifecycle
-    
-    init(_ text: String) {
-        super.init(frame: .zero)
-        setTitle(text, for: .normal)
-        setupViews()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: Custom funcs
-
-    private func setupViews() {
-        setTitleColor(UIColor(named: "textColor"), for: .normal)
-        backgroundColor = UIColor(named: "backgroundColor")
-        layer.cornerRadius = .largeCornerRadius
-        layer.borderWidth = 1
-        layer.borderColor = UIColor.hpLightGray.cgColor
-        anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: .extraLargeSpace, height: .extraLargeSpace)
-    }
-
-}
