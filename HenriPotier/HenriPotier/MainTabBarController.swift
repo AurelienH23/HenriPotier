3947556d8321dde4699bfaefc2b0f17ee8f9964b
Aurélien Haie <>
2021-01-17 20:58:45 +0100

    [REFACTOR] MainTabBarController

--- HenriPotier/HenriPotier/MainTabBarController.swift
+++ HenriPotier/HenriPotier/MainTabBarController.swift
@@ -14,21 +14,28 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupControllers()
+        setupTabBar()
     }
 
     // MARK: Custom funcs
 
     private func setupViews() {
         view.backgroundColor = UIColor(named: "backgroundColor")
+    }
 
+    private func setupControllers() {
         let libraryController = LibraryViewController()
         libraryController.tabBarItem = UITabBarItem(title: "Librairie", image: UIImage(systemName: "book.closed"), selectedImage: UIImage(systemName: "book.closed.fill"))
         let cartViewController = CartViewController()
         cartViewController.tabBarItem = UITabBarItem(title: "Panier", image: UIImage(systemName: "cart"), selectedImage: UIImage(systemName: "cart.fill"))
         viewControllers = [libraryController, cartViewController]
+    }
 
+    private func setupTabBar() {
         tabBar.barTintColor = UIColor(named: "backgroundColor")
         tabBar.tintColor = .hpGreen
+        tabBar.unselectedItemTintColor = UIColor(named: "textColor")
     }
 
 }
